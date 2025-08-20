91709946a9726a2eec2567c7be01b7f308a446cc
ashfak <ashfak123ashfak@gmail.com>
2025-08-20 14:29:08 +0530

    2nd commit

--- backend/manage.py
+++ backend/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+#teesthhffjhgfjhfkhfkhfkhfhggfjgdgdhghgdhgdhgdgd
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wellness_api.settings")
